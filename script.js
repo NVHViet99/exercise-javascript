54603a1dc942b0476691e0f99255f6447faf7590
NVHViet99 <hoangviet220699@gmail.com>
2022-03-07 11:03:18 +0700

    JavaScript exercises in chapter 2

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-const Mark = {
-  fullName: "Mark Miller",
-  mass: 7,
-  height: 1.69,
-  calcBMI: function () {
-    return (this.bmi = this.mass / this.height ** 2);
-  },
-};
+// Looping a Triangle
+let value = "#";
+while (value.length < 7) {
+  console.log(value);
 
-const John = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    return (this.bmi = this.mass / this.height ** 2);
-  },
-};
-
-Mark.calcBMI();
-John.calcBMI();
-
-console.log(Mark.bmi, John.bmi);
-
-if (Mark.bmi > John.bmi) {
-  console.log(
-    `${Mark.fullName}'s BMI (${Mark.bmi}) is higher than ${John.fullName}'s (${John.bmi})`
-  );
-} else if (Mark.bmi < John.bmi) {
-  console.log(
-    `${Mark.fullName}'s BMI (${Mark.bmi}) is lower than ${John.fullName}'s (${John.bmi})`
-  );
+  value = value + "#";
 }
+
+// FizzBuzz
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 == 0) {
+    console.log("frizz");
+  } else if (i % 5 == 0) {
+    console.log("buzz");
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("frizzBuzz");
+  } else {
+    console.log(i);
+  }
+}
+
+//checssBoard
+const chessBoard = function (num) {
+  let output = "";
+
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      (i + j) % 2 == 0 ? (output += " ") : (output += "#");
+    }
+    output += "\n";
+  }
+  console.log(output);
+};
+
+chessBoard(5);
